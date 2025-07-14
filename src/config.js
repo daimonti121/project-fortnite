7bf838b54a6fede16a033e2a79e5058e2adac0fd
Yushenko Dima <damios.1812@gmail.com>
2025-07-14 17:56:00 +0200

    first commit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+// const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = 'c8d4a093-9aaa1af9-fbbbd8f8-57f2c29a';
+
+const API_URL = 'https://fortniteapi.io/v1/shop?lang=ru';
+
+export { API_KEY, API_URL };
